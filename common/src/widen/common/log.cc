a91d5f76660400674285553b43ca4eb6ec2f707e
Vincent <kmint1819@gmail.com>
2022-12-30 11:59:08 -0600

    added thread id in log format

--- common/src/widen/common/log.cc
+++ common/src/widen/common/log.cc
@@ -14,8 +14,8 @@ namespace widen
         logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
         logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("widen.log", true));
 
-        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+        logSinks[0]->set_pattern("%^[%T] [%t] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%t] [%l] %n: %v");
 
         logger = std::make_shared<spdlog::async_logger>("WIDEN", logSinks.begin(), logSinks.end(), spdlog::thread_pool());
         spdlog::register_logger(logger);
